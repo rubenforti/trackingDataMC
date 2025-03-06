d96bfcdd19c8e6234a04c9c067182f73a491d9dd
Ruben Forti <rubenforti27@gmail.com>
2025-03-06 19:18:45 +0100

    fixed pile-up and nvtx corrections

--- Scripts/vertexreweight.C
+++ Scripts/vertexreweight.C
@@ -18,20 +18,20 @@ typedef unsigned int uint;
 
 void pileupreweight() {
 //  TFile* file1 = TFile::Open("/afs/cern.ch/work/p/ppalit/public/Data_MC/forgitpush/CMSSW_10_6_2/src/DQM/TrackingMonitorSource/Jobs/Run2017_UL/data/SingleMu/DQM_V0001_R000278509__Global__CMSSW_X_Y_Z__RECO.root");
-  TFile* file1 = TFile::Open("/afs/cern.ch/work/d/dbruschi/TrackingPOG/Copy/CMSSW_13_0_11/src/DQM/TrackingMonitorSource/test/13p6_Run2023C/pileUp_2024I_Golden.root");  
+  TFile* file1 = TFile::Open("/afs/cern.ch/work/d/dbruschi/TrackingPOG/Copy/CMSSW_13_0_11/src/DQM/TrackingMonitorSource/test/13p6_Run2023C/DQM_V0001_R000999999__Global__CMSSW_X_Y_Z__RECO.root");  
   file1->ls();  
 //  TFile* file2 = TFile::Open("/afs/cern.ch/work/p/ppalit/public/Data_MC/forgitpush/CMSSW_10_6_2/src/DQM/TrackingMonitorSource/Jobs/Run2017_UL/data/ZtoMM/nopuroot/DQM_V0001_R000000001__Global__CMSSW_X_Y_Z__RECO.root");
   TFile* file2 = TFile::Open("/afs/cern.ch/work/d/dbruschi/TrackingPOG/CMSSW_13_0_12/src/DQM/TrackingMonitorSource/test/MC/RelValSingleNu/C/DQM_V0001_R000000001__Global__CMSSW_X_Y_Z__RECO.root");
   file2->ls(); 
  
-  TH1F* h1 = dynamic_cast<TH1F*>(file1->Get("pileup"));	
+  TH1F* h1 = dynamic_cast<TH1F*>(file1->Get("DQMData/Run 999999/StandaloneTrackMonitor/Run summary/highPurityTracks/nVertex"));	
 //  TH1F* h1 = dynamic_cast<TH1F*>(file1->Get("DQMData/Run 278509/StandaloneTrackMonitor/Run summary/ElectronTracks/nVertex"));
-  h1->SetName("pileup_data");
+  h1->SetName("nvertex_data");
   cout << "1st histo " << h1->GetName() << " opened successfully" << endl;
 
-  TH1F* h2 = dynamic_cast<TH1F*>(file2->Get("DQMData/Run 1/StandaloneTrackMonitor/Run summary/highPurityTracks/trueNInt"));
+  TH1F* h2 = dynamic_cast<TH1F*>(file2->Get("DQMData/Run 1/StandaloneTrackMonitor/Run summary/highPurityTracks/nVertex"));
   //TH1F* h2 = dynamic_cast<TH1F*>(file2->Get("DQMData/Run 1/StandaloneTrackMonitor/Run summary/ElectronTracks/nVertex"));
-  h2->SetName("pileup_mc");
+  h2->SetName("nvertex_mc");
   cout << "2nd histo " << h2->GetName() << " opened successfully" << endl;
 
   cout << h1->Integral() << ", " << h2->Integral() << endl;
@@ -56,7 +56,7 @@ void pileupreweight() {
 
   cout << nbinsx2 << ";" << xmin2 << ";" << xmax2 << endl;
 
-  TH1F* hn = new TH1F("pileupweight", "Pile up Weight Factor", 120, 0, 120);
+  TH1F* hn = new TH1F("nvertexweight", "Reco::Vertex Weight Factor", 120, 0, 120);
   for (int j = 1; j <= nbinsx1; ++j) {
     double v1 = h1->GetBinContent(j);
     double v2 = h2->GetBinContent(j);
@@ -65,7 +65,7 @@ void pileupreweight() {
     hn->SetBinContent(j,rat);
   } 
  
-  TFile* f1 = TFile::Open("/afs/cern.ch/work/d/dbruschi/TrackingPOG/CMSSW_13_0_12/src/DQM/TrackingMonitorSource/test/MC/RelValSingleNu/C/PileupScaleFactor_999999_wrt_trueNInt_Run2023C.root", "RECREATE");
+  TFile* f1 = TFile::Open("/afs/cern.ch/work/d/dbruschi/TrackingPOG/CMSSW_13_0_12/src/DQM/TrackingMonitorSource/test/MC/RelValSingleNu/C/PileupScaleFactor_999999_wrt_nVertex_Run2023C.root", "RECREATE");
   hn->Write();
   f1->Write();
   f1->Close();
